d81fe1bfaa09022d18ef7920541892584f0e49db
Desi Curry <Blackice@Desis-MacBook-Air.local>
2020-01-29 22:55:37 -0500

    Made some changes and attempted stretch

--- src/App.js
+++ src/App.js
@@ -30,12 +30,16 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={ () => {setHomeScore(homeScore + 7)}}>Home Touchdown</button>
+          <button className="homeButtons__touchdown" onClick={ () => {setHomeScore(homeScore + 6)}}>Home Touchdown</button>
+          <button className='homeButtons_extraPoint' onClick={() => setHomeScore(homeScore + 1)}>Home Extra Point</button>
           <button className="homeButtons__fieldGoal" onClick={ () => {setHomeScore(homeScore + 3)}}>Home Field Goal</button>
+          <button className='homeButtons_twoPoints' onClick={() => setHomeScore(homeScore + 2)}>Home Two Points</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={ () => {setAwayScore(awayScore + 7)}}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={ () => {setAwayScore(awayScore +3 )}}>Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={ () => {setAwayScore(awayScore + 6)}}>Away Touchdown</button>
+          <button className='awayButtons_extraPoint' onClick={() => setAwayScore(awayScore + 1)}>Away Extra Point</button>
+          <button className="awayButtons__fieldGoal" onClick={ () => {setAwayScore(awayScore + 3)}}>Away Field Goal</button>
+          <button className='awayButtons_twoPoints' onClick={() => setAwayScore(awayScore + 2)}>Home Two Points</button>
         </div>
       </section>
     </div>
